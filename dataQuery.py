394eeb419a15015fbc32b1d221dcaae312a92ac8
Nongma Sorgho <57603029+Atepir@users.noreply.github.com>
2020-05-02 19:04:22 +0200

    adding functions specifications

--- dataQuery.py
+++ dataQuery.py
@@ -3,7 +3,7 @@
 import sqlite3
 import csv
 
-with open('vol_conso_totale.csv', encoding='utf8') as File:
+with open('conso.csv', encoding='utf8') as File:
     reader = csv.reader(File, delimiter=';')
     listed_data = []
     for line in reader:
@@ -17,29 +17,65 @@ c = connexion.cursor()
 
 # Creating the tables and grepping the data
 
-#c.execute('CREATE TABLE paliers (palier int, debut int, fin int, pourcentage float, '
-#          'total int)')
-for i in range(len(listed_data)):
-    vy_listed_data = listed_data[i][0].split(',')
-    c.execute('INSERT INTO paliers VALUES ({}, {}, {}, {}, {})'.format(vy_listed_data[0],
-                                                                       vy_listed_data[1],
-                                                                       vy_listed_data[2],
-                                                                       vy_listed_data[3],
-                                                                       vy_listed_data[4]
-                                                                       ))
+def createTablePaliers():
+    """
+    :param: none
+    :return: creates a table named paliers in the database
+    """
+    c.execute('CREATE TABLE paliers (palier int, debut int, fin int, pourcentage float, total int)')
 
-# c.execute('CREATE TABLE conso (debut int, fin int, float conso_totale)')
-for i in range(len(listed_data)):
-    tri_liste = listed_data[i][0].split(',')
-    c.execute('INSERT INTO conso VALUES ({}, {}, {})'.format(tri_liste[0],
-                                                             tri_liste[1],
-                                                             tri_liste[2]))
+def insertPaliers(listed_data):
+    """
+    :param listed_data: csv file which contains the paliers data as a 5 columns table of respectively integer,
+                        integer, integer, float and integer values
+    :return: creates a row in the table paliers
+    """
+    for i in range(len(listed_data)):
+        vy_listed_data = listed_data[i][0].split(',')
+        c.execute('INSERT INTO paliers VALUES ({}, {}, {}, {}, {})'.format(vy_listed_data[0],
+                                                                           vy_listed_data[1],
+                                                                           vy_listed_data[2],
+                                                                           vy_listed_data[3],
+                                                                           vy_listed_data[4]
+                                                                           ))
 
-for a in c.execute('SELECT * FROM conso'):
-    print(a)
+def createTableConso():
+    """
+    :param: none
+    :return: creates a table named conso in the database
+    """
+    c.execute('CREATE TABLE conso (debut int, fin int, conso_totale float, debit float)')
 
-for b in c.execute('SELECT * FROM palier'):
-    print(b)
+def insertConso(listed_data):
+    """
+        :param listed_data: csv file which contains the data of conso under the form of a 4 rows table which have
+                            respectively the values of an integer, integer, float and float
+        :return: creates a row in the table paliers
+        """
+    for i in range(len(listed_data)):
+        tri_liste = listed_data[i][0].split(',')
+        c.execute('INSERT INTO conso VALUES ({}, {}, {}, {})'.format(tri_liste[0],
+                                                                 tri_liste[1],
+                                                                 tri_liste[2],
+                                                                     tri_liste[3]))
+
+def showConso():
+    """
+    :param: none
+    :return: prints the data stored in the table conso
+    """
+    for a in c.execute('SELECT * FROM conso'):
+        print(a)
+
+def showPaliers():
+    """
+    :param: none
+    :return: prints the data stored in the table paliers
+    """
+    for b in c.execute('SELECT * FROM paliers'):
+        print(b)
+
+
 
 connexion.commit()
 
